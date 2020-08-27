c8194040409a89c9650b03a51b7b3ae9592827d4
BenKapoor <benjaminkapoor@yahoo.fr>
2020-08-27 16:51:02 +0200

    ajouts des données pour les classes et leur descirption

--- dofus-api/src/main/java/com/dofusapi/domaine/Classe.java
+++ dofus-api/src/main/java/com/dofusapi/domaine/Classe.java
@@ -42,5 +42,94 @@ public class Classe {
 	
 	private String images_masc;
 	private String images_fem;
+	/**
+	 * @param ankamaId
+	 * @param nom
+	 * @param url_off
+	 * @param description
+	 * @param rattacherRoles
+	 * @param sorts
+	 * @param images_masc
+	 * @param images_fem
+	 */
+	public Classe(int ankamaId, String nom, String url_off, Description description, Set<Role> rattacherRoles,
+			Collection<Sort> sorts, String images_masc, String images_fem) {
+		super();
+		this.ankamaId = ankamaId;
+		this.nom = nom;
+		this.url_off = url_off;
+		this.description = description;
+		this.rattacherRoles = rattacherRoles;
+		this.sorts = sorts;
+		this.images_masc = images_masc;
+		this.images_fem = images_fem;
+	}
+	/**
+	 * 
+	 */
+	public Classe() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	@Override
+	public String toString() {
+		return "Classe [ankamaId=" + ankamaId + ", nom=" + nom + ", url_off=" + url_off + ", description=" + description
+				+ ", rattacherRoles=" + rattacherRoles + ", sorts=" + sorts + ", images_masc=" + images_masc
+				+ ", images_fem=" + images_fem + "]";
+	}
 	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getAnkamaId() {
+		return ankamaId;
+	}
+	public void setAnkamaId(int ankamaId) {
+		this.ankamaId = ankamaId;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getUrl_off() {
+		return url_off;
+	}
+	public void setUrl_off(String url_off) {
+		this.url_off = url_off;
+	}
+	public Description getDescription() {
+		return description;
+	}
+	public void setDescription(Description description) {
+		this.description = description;
+	}
+	public Set<Role> getRattacherRoles() {
+		return rattacherRoles;
+	}
+	public void setRattacherRoles(Set<Role> rattacherRoles) {
+		this.rattacherRoles = rattacherRoles;
+	}
+	public Collection<Sort> getSorts() {
+		return sorts;
+	}
+	public void setSorts(Collection<Sort> sorts) {
+		this.sorts = sorts;
+	}
+	public String getImages_masc() {
+		return images_masc;
+	}
+	public void setImages_masc(String images_masc) {
+		this.images_masc = images_masc;
+	}
+	public String getImages_fem() {
+		return images_fem;
+	}
+	public void setImages_fem(String images_fem) {
+		this.images_fem = images_fem;
+	}	
 }
